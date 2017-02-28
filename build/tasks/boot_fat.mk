e636aa4a724ee374050ce13e12e67b76dc7539ee
Joakim Bech <joakim.bech@linaro.org>
2017-02-28 09:08:59 +0100

    boot: use a clean boot_fat.uefi.img
    Copy the clean boot_fat.uefi.img. The reason for having a pre-generated
    boot_fat.uefi.img here is because on newer versions of mkfs.fat (4.0),
    there it seems impossible to set the "Reserved vectors" to "1". It
    defaults to 4 reserved sectors and it can go higher but not lower (-R
    flag). I could be a bug in the new mkfs.fat tool. So, the pre-build
    image here some from the mkfs.fat 3.0.26 (2014-03-07), which gives the
    correct number of reserved sectors. This replace the "dd" and the
    "mkfs.fat" steps previously done here.
    
    Change-Id: I7bf53cc4d9c619363310387531f24e109e9afc4f
    Signed-off-by: Joakim Bech <joakim.bech@linaro.org>

--- build/tasks/boot_fat.mk
+++ build/tasks/boot_fat.mk
@@ -3,8 +3,19 @@ ifneq ($(filter hikey%, $(TARGET_DEVICE)),)
 ifeq ($(TARGET_BOOTIMAGE_USE_FAT), true)
 $(PRODUCT_OUT)/boot_fat.uefi.img: $(INSTALLED_KERNEL_TARGET) $(INSTALLED_RAMDISK_TARGET) $(PRODUCT_OUT)/hi6220-hikey.dtb
 # $@ is referring to $(PRODUCT_OUT)/boot_fat.uefi.img
-	dd if=/dev/zero of=$@ bs=512 count=98304
-	mkfs.fat -n "boot" $@
+	#dd if=/dev/zero of=$@ bs=512 count=98304
+	#mkfs.fat -n "boot" $@
+	# Copy the clean boot_fat.uefi.img. The reason for having a
+	# pre-generated boot_fat.uefi.img here is because on newer versions of
+	# mkfs.fat (4.0), then it seems impossible to set the "Reserved vectors"
+	# to "1". It defaults to 4 reserved sectors and it can go higher but not
+	# lower (-R flag). I could be a bug in the new mkfs.fat tool. So, the
+	# pre-build image here some from the mkfs.fat 3.0.26 (2014-03-07), which
+	# gives the correct number of reserved sectors. This replace the "dd"
+	# and the "mkfs.fat" steps previously done here.
+	#
+	# Always start out with a fresh copy
+	cp -f device/linaro/hikey/installer/hikey/boot_fat.uefi.img $(PRODUCT_OUT)/
 	$(FAT16COPY) $@ $(PRODUCT_OUT)/kernel
 	$(FAT16COPY) $@ $(PRODUCT_OUT)/hi6220-hikey.dtb
 	$(FAT16COPY) $@ device/linaro/hikey/bootloader/*
